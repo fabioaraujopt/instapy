56a52048b1d59c99d2f6d58e8ead53ff5da01dd2
fabioaraujopt <31209502+fabioaraujopt@users.noreply.github.com>
2020-09-23 21:47:20 +0100

    add file

---
+++ docker-compose/docker_quickstart.py
@@ -0,0 +1,53 @@
+"""
+This template is written by @cormo1990
+
+What does this quickstart script aim to do?
+- Basic follow/unfollow activity.
+
+NOTES:
+- I don't want to automate comment and too much likes because I want to do
+this only for post that I really like the content so at the moment I only
+use the function follow/unfollow.
+- I use two files "quickstart", one for follow and one for unfollow.
+- I noticed that the most important thing is that the account from where I
+get followers has similar contents to mine in order to be sure that my
+content could be appreciated. After the following step, I start unfollowing
+the user that don't followed me back.
+- At the end I clean my account unfollowing all the users followed with
+InstaPy.
+"""
+
+# imports
+from instapy import InstaPy
+from instapy import smart_run
+import time
+# login credentials
+insta_username = 'passaro.manidestro'
+insta_password = 'coelho123'
+
+# get an InstaPy session!
+# set headless_browser=True to run InstaPy in the background
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=True)
+
+with smart_run(session):
+    session.set_quota_supervisor(enabled=True, sleep_after=["likes", "comments_d", "follows", "unfollows", "server_calls_h"], sleepyhead=True, stochastic_flow=True, notify_me=True,
+                              peak_likes_hourly=57,
+                              peak_likes_daily=585,
+                               peak_comments_hourly=21,
+                               peak_comments_daily=182,
+                                peak_follows_hourly=48,
+                                peak_follows_daily=None,
+                                 peak_unfollows_hourly=35,
+                                 peak_unfollows_daily=402,
+                                  peak_server_calls_hourly=None,
+                                  peak_server_calls_daily=4700)
+
+    
+    while(True): # or xrange if you are on 2.X
+        time.sleep(200)
+        session.follow_user_following(['tvioficial'], amount=10, randomize=False, sleep_delay=600)
+    
+
+
